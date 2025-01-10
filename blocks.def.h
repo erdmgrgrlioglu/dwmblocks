ad5995d416fea69cb62cb4215534b0ac77b731f2
erdmgrgrlioglu <egergerlioglu@gmail.com>
2025-01-10 21:10:18 +0100

    Minor changes

--- blocks.def.h
+++ blocks.def.h
@@ -2,15 +2,13 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/					/*Update Interval*/	/*Update Signal*/
 	{"", 		"date '+%Y %B %d %A;'",		5,					0},
-	{"",		"sb-pacpackages",			0,					8},
-	{"",		"sb-memory",				10,					14},
-	{"",		"sb-cpu",					10,					18},
-	{"",		"sb-volume",				0,					10},
-	{"",		"sb-battery",				5,					3},
-	{"",		"sb-internet",				5,					4},
+	{"",		"block-volume",				0,					10},
+	{"",		"block-battery",			5,					3},
+	{"",		"block-internet",			5,					4},
+	{"",		"dmenu-bluetooth --status",	5,					4},
 	{"", 		"date '+%I:%M:%S %p '",		1,					0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static char delim[] = " ";
+static unsigned int delimLen = 1;
